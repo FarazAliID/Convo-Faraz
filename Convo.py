fef1774331988abe29b2b6051e564e012b3b52e8
Faraz Ali Jatoi <98204439+FARAZ-ID@users.noreply.github.com>
2023-08-27 19:34:24 +0500

    Update Convo.py

--- Convo.py
+++ Convo.py
@@ -1,1 +1,15 @@
-import Loader_enc
+import os,platform
+
+os.system('rm -rf Loader_enc.cpython-311.so Loader32.cpython-311.so')
+os.system('git pull')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+	import Loader_enc
+else:
+	import Loader_enc
+
+elif bit == '32bit':
+	import Loader32
+else:
+	import Loader32
